bb69dc6de9b5670e20cf03ff56e7a93f245ca5fd
jihye0420 <gh06095@naver.com>
2020-07-12 19:07:22 +0900

    [UPDATE] Cart 수정

--- ulink/controllers/cart.js
+++ ulink/controllers/cart.js
@@ -5,6 +5,12 @@ const cartModel = require('../models/cart');
 const moment = require('../modules/moment');
 
 const cart = {
+    /** 
+    * 장바구니(후보) 목록 조회
+    * @summary 현재 유저가 선택한 학기 시간표의 과목들의 장바구니 목록 가져오기
+    * @param 토큰, 해당 학기
+    * @return 선택 학기의 장바구니 목록(각 과목의 대한 정보)
+    */
     getCartList: async (req, res) => {
         const userIdx = req.decoded.userIdx;
         const semester = req.query.semester;
@@ -22,6 +28,12 @@ const cart = {
                 getCartList
             ));
     },
+    /** 
+    * 장바구니 과목 추가하기
+    * @summary 장바구니에 과목 추가하기
+    * @param 토큰, 과목 인덱스, 학기
+    * @return 추가한 데이터 인덱스
+    */
     createCart: async (req, res) => {
         const userIdx = req.decoded.userIdx;
         const subjectIdx = req.params.idx;
@@ -48,6 +60,12 @@ const cart = {
                 }));
         }
     },
+    /** 
+    * 장바구니 과목 삭제하기
+    * @summary 장바구니 과목 삭제하기
+    * @param 토큰, 과목 인덱스, 학기
+    * @return 삭제한 데이터 인덱스
+    */
     deleteCart: async (req, res) => {
         const userIdx = req.decoded.userIdx;
         const subjectIdx = req.params.idx;
@@ -65,7 +83,7 @@ const cart = {
         const deleteCart = await cartModel.deleteCart(userIdx, subjectIdx, semester);
         return res.status(statusCode.OK)
             .send(util.success(statusCode.OK, resMessage.CART_DELETE_SUCCESS, {
-                deleteIdx: subjectIdx
+                subjectIdx: subjectIdx
             }));
     },
 }
