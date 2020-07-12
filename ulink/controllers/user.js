7c8c9d9951bcf52def692de95c7dbf4f3f125a67
KimDoubleB <tree9295@gmail.com>
2020-07-12 12:39:35 +0900

    [UPDATE]  Sign up에gender 추가

--- ulink/controllers/user.js
+++ ulink/controllers/user.js
@@ -10,7 +10,7 @@ const user = {
     /** 
     * 회원가입하기
     * @summary 주어진 정보들을 가지고 회원가입
-    * @param 아이디, 비밀번호, 이름, 이메일, 학교명
+    * @param 아이디, 비밀번호, 이름, 이메일, 학교명, 성별(m/f)
     * @return 회원가입한 유저의 아이디, 인덱스
     */
     signUp: async (req, res) => {
@@ -19,11 +19,12 @@ const user = {
             password,
             name,
             email,
-            school
+            school,
+            gender
         } = req.body;
 
         // empty value
-        if (!id || !password || !name || !email || !school) {
+        if (!id || !password || !name || !email || !school || !gender) {
             return res.status(statusCode.BAD_REQUEST)
                 .send(util.fail(statusCode.BAD_REQUEST, resMessage.NULL_VALUE));
         }
@@ -44,7 +45,7 @@ const user = {
         const encryptPassword = encrypt.encryption(password, salt);
         const nickname = nameMaker.makeRandomName();
 
-        const idx = await userModel.signUp(id, encryptPassword, salt, name, email, nickname, school);
+        const idx = await userModel.signUp(id, encryptPassword, salt, name, email, nickname, school, gender);
         if (idx === -1) {
             return res.status(statusCode.DB_ERROR)
                 .send(util.fail(statusCode.DB_ERROR, resMessage.DB_ERROR));
